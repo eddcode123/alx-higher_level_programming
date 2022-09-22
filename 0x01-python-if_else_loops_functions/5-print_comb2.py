a4f6d21ca063752f72281d3fa64eef1a21c7cf02
eddcode123 <edwinmutugi29@gmail.com>
2022-09-22 11:09:25 +0300

    addingcorrection

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(0, 100):
-    print('{0:0=2d}'.format(i), end=', ')
-else:
-    print(end='\n')
+for i in range(0, 99):
+    print('{:0=2d}'.format(i), end=', ')
+print("{:02d}".format(i + 1))

6b9ee4528c7200533e4913176f55d764dcec54e2
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-21 17:28:47 +0300

    addingfiles

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+def print_last_digit(number):
+    lastdigit = 0
+    if number < 0:
+        lastdigit = (number * -1) % 10
+    else:
+        lastdigit = number % 10
+    print("{}".format(lastdigit), end="")
+
+    return (lastdigit)

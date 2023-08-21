5fc1305e5122e7ca2368e902efd5f8fd1b3a2d4b
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-21 13:02:33 +0300

    addingfiles

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-# use a for loop and range to print alphabet
-for i in range(97, 123):
+# use a loop to print alphabet
+i = 97
+while i < 123:
     print("{}".format(chr(i)), end="")
+    i += 1

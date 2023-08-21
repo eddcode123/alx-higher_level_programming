6091fc5e2818d68222206d5d5b0b10f5305680d4
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-21 13:22:14 +0300

    addingfiles

---
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# store first number in a variable
+num = 0
+# loop and print 0 - 98 in decimal and hexidecimal
+while num < 99:
+    print("{} = {}".format(num, hex(num)))
+    num += 1

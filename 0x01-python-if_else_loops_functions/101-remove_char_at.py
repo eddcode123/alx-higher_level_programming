92bc5e33900826c6fc326134c4fa30ab6374bb9f
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-22 09:56:30 +0300

    addingfiles

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/pyhton3
+def remove_char_at(str, n):
+    newstr = ""
+    if (n < 0):
+        return str
+    strlen = len(str)
+    if (n > strlen):
+        return str
+    for i in range(strlen):
+        if (i != n):
+            newstr = newstr + str[i]
+        i += 1
+    return newstr

caa394edb6c0eb48aa6a61a3600a31ef8d83ee53
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-23 16:02:24 +0300

    addingfiles

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,6 +3,6 @@ def element_at(my_list, idx):
     # find length of list
     listlen = len(my_list)
     # check if idx is negative or greater than len of list
-    if idx < 0 or idx > listlen:
+    if idx < 0 or idx >= listlen:
         return None
     return my_list[idx]

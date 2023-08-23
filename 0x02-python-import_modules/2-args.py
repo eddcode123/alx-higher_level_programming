8fda99b644d86b27801a70e14beed10b3b79a669
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-23 10:23:52 +0300

    addingfiles

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,10 @@ if __name__ == "__main__":
     from sys import argv
     argslen = len(argv) - 1
     if argslen == 0:
-        print("{} arguments.".format(argslen))
-    elif (argslen == 1):
-        print("{} argument:".format(argslen))
-        print("{}: {}".format(argslen, argv[argslen]))
-    if argslen > 1:
+        print("0 arguments.")
+    elif argslen == 1:
+        print("1 argument:")
+    else:
         print("{} arguments:".format(argslen))
-        for i in range(1, argslen):
-            print("{}: {}".format(i, argv[i]))
+    for i in range(1, argslen + 1):
+        print("{}: {}".format(i, argv[i]))

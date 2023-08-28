f1bb79a67b51840c17996b6f56411691fa09009d
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-28 10:50:05 +0300

    adding files

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,7 +3,10 @@ def multiple_returns(sentence):
     # declare an empty tuple
     new_tuple = ()
     # check for a empty string
-    if (sentence):
+    if (sentence == ""):
+        strlen = 0
+        char = None
+    else:
         strlen = len(sentence)
         char = sentence[0]
     new_tuple = (strlen, char)

d2aba85780f22fcc9fc79f013764c480e329b551
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-29 15:53:04 +0300

    adding files

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -2,7 +2,7 @@
 def update_dictionary(a_dictionary, key, value):
     # check if key exist
     if key in a_dictionary:
-        a_dictionary[key] = value # update value at key
+        a_dictionary[key] = value  # update value at key
     else:
-        a_dictionary[key] = value # add key - value pair
+        a_dictionary[key] = value  # add key - value pair
     return (a_dictionary)

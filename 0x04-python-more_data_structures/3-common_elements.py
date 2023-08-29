46584c61513d68bea355f1a653384aef16759db9
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-29 13:11:02 +0300

    adding files

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
+    common = None
     # check is set is not empty
     if (set_1 and set_2):
-        return (set_1 & set_2)
+        common = set_1 & set_2
+
+    return (common)

2ee6442cb619fabd5adb33dd330aa25485ad1b99
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-29 12:17:24 +0300

    adding files

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    # create an empty list
-    newlist = []
-    # check if list is empty
-    if (matrix):
-        # assign squared values of list to newlist
-        newlist = [item ** 2 if isinstance(item, int) else [value ** 2 for value in item] for item in matrix]
+    # create a copy of matrix
+    newmatrix = matrix.copy()
 
-    return (newlist)
+    # iterlate the newmatrix and square its values
+    for i in range(len(matrix)):
+        newmatrix[i] = list(map(lambda item: item ** 2, matrix[i]))
+
+    return (newmatrix)

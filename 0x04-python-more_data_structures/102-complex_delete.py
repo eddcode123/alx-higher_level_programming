4a56f6bd6b98edf1b452112fda6e181f6885d476
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-08-30 11:20:36 +0300

    adding files

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,6 +4,6 @@ def complex_delete(a_dictionary, value):
     if (a_dictionary):
         for k, v in a_dictionary.items():
             if v == value:
-                del a_dictionary[k];
+                del a_dictionary[k]
 
     return (a_dictionary)

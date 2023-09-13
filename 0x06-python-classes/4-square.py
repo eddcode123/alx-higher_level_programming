ad52c4af55d1ea2509403d776a98e2ccc9bd733b
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-09-13 13:27:54 +0300

    addingfiles

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Represent a square."""
+
+    def __init__(self, size=0):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+        def area(self):
+            """return  area of the square."""
+            self.area = (self.__size * self.__size)
+            return (self.area)
+
+        def size(self):
+            """Updates the size"""
+            self.__size = size

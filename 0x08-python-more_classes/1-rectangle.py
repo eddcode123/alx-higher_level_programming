8b6cd7025d7bc09489d71aea23392b1e12773b6a
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-09-18 17:13:47 +0300

    addingfiles

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Define a rectangle"""
+
+
+class Rectangle:
+    """represent a Rectangle."""
+    def __init__(self, width=0, height=0):
+        """initializing a rectangle
+
+        Args:
+        width (int): width of a rectangle.
+        height(int): height of a rectamgle.
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def height(self):
+        """Get height of private instance"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        """set value to private instance height"""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        elif self.__height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        """Get  width of private instance"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        """Set value to private instance width"""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif self.__width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value

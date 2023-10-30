01296b184eb91406a8bcad6ea3d3bdce9d6b4519
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2023-10-30 09:52:02 +0300

    addingfiles

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -6,7 +6,7 @@ def search_replace(my_list, search, replace):
     # iterate list if not empty and check and replace element
     if (my_list):
         for item in my_list:
-            # check if search is in the list
+            # check if item exist in the list
             if (item == search):
                 newlist.append(replace)
             else:

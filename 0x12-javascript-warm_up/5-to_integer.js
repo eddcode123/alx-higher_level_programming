867039eff6f810e4aae8f50b6a859627f6158b07
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-13 02:26:05 +0300

    adding javacode

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+const args = process.argv;
+const number = parseInt(args[2]);
+if (!isNaN(number)) {
+  console.log('My number: ', number);
+} else {
+  console.log('Not a number');
+}

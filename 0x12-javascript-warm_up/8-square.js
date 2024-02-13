713230e312d20e7f544339f6043bba2dbdf7926b
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-13 03:15:46 +0300

    adding javacode

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const args = process.argv;
 const number = parseInt(args[2]);
-if (!isNaN(number) && number >= 0) {
+if (!isNaN(number)) {
   for (let x = 0; x < number; x++) {
     let row = '';
     for (let y = 0; y < number; y++) {

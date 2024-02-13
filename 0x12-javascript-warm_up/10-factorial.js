17952a7254884d197084dd7deb2f0d638eaa4226
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-13 11:37:34 +0300

    function to calculate fuctorial of number

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,9 @@
 #!/usr/bin/node
-const factorial = (num) => {
-  if (isNaN(num)) {
-    return 1;
-  }
-  if (num === 0) {
+function factorial (num) {
+  if (isNaN(num) || num === 1) {
     return 1;
   }
   return num * factorial(num - 1);
-};
+}
 const num = parseInt(process.argv[2]);
 console.log(factorial(num));

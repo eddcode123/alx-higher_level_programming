b1ce73fb46f0e7455570d4ae049c5f907bf17940
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-13 12:55:10 +0300

    updating value of an object variable

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 function factorial (num) {
-  if (isNaN(num) || num === 1) {
+  if (Number.isNaN(num) || num === 1) {
     return 1;
   }
-  return num * factorial(num - 1);
+  return (num * factorial(num - 1));
 }
 const num = parseInt(process.argv[2]);
 console.log(factorial(num));

803302462561e19a513dbcae7a5986d31ba07f30
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-13 03:14:59 +0300

    adding javacode

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 const args = process.argv;
 const number = parseInt(args[2]);
-
 if (!isNaN(number) && number >= 0) {
   for (let x = 0; x < number; x++) {
     let row = '';
     for (let y = 0; y < number; y++) {
-      row += 'x'; // Append 'x' to the row string
+      row += 'x';
     }
-    console.log(row); // Print the row
+    console.log(row);
   }
 } else {
   console.log('Missing size');

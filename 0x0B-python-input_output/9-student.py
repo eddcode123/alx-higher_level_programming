44fea5cf6eae7f3d1f1ed90a3c40e36e712f43d0
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-16 20:59:07 +0300

    pyscript to return dict representation of a Student class

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""" Define a student class """
+
+
+class Student():
+    """ Represent a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initialize a student.
+
+        Args:
+        first_name (str): first name of astudent
+        last_name (str): last name of a student
+        age (int): age of the student
+
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, Stude):
+        """ returns a dictionary representation of a student """
+        return Student.__dict__

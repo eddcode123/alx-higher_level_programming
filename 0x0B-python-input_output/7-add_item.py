221d76ee01dd2b6bda4c2a0a8dc4978a15cdf038
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-16 16:14:19 +0300

    pyscript writes a json file from commad args and then reads file in py obj

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,18 +7,9 @@ if __name__ == "__main__":
     load_from_json_file = \
         __import__('6-load_from_json_file').load_from_json_file
 
-# create a list of arguments
-arguments = sys.argv
-
-# if arguments is 1 then make arguments a empty list
-if len(arguments) == 1:
-    arguments = []
-else:
-    # make arguments a list of arguments passed
-    arguments = sys.argv[1:]
-
-# save list of arguments to a json file
-save_to_json_file(arguments, 'add_item.json')
-
-# load the saved json files to python object
-load_from_json_file('add_item.json')
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")

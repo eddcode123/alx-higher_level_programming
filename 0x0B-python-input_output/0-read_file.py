54e1d4c356d060b6ead3d128e782b7edb48475cc
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-16 14:04:34 +0300

    function reads and prints content of a file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python
 
 def read_file(filename=""):
-    # Open the file specified by filename in read mode ('r') with UTF-8 encoding
+    # Open filename in read mode ('r') with UTF-8 encoding
     with open(filename, 'r', encoding='utf-8') as f:
-        # Read the contents of the file and store it in the variable 'data'
+        # Read file contents and store it in 'data'
         data = f.read()
-    # Print the contents of the file to the console
+    # Print file contents to STDOUT
     print(data)

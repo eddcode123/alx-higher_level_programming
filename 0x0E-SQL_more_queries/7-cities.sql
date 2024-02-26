e3ebb6a19aa8b1cb2bbd121a764a7b15e9cebde7
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-26 13:40:26 +0300

    adding script that creates the database hbtn_0d_usa and the table cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,11 +1,8 @@
 -- script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on a MySQL server
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-
--- create a table in the created data base
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
-    id INT UNIQUE NOT NULL AUTO_INCREMENT,
-    name VARCHAR(256) NOT NULL,
-    state_id INT NOT NULL,
-    PRIMARY KEY (id),
-    FOREIGN KEY (state_id) REFERENCES hbtn_0d_usa.states(id)
-);
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities` (
+  `id` INT UNIQUE NOT NULL AUTO_INCREMENT,
+  `state_id` INT NOT NULL,
+  `name` VARCHAR(256) NOT NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`state_id`) REFERENCES `hbtn_0d_usa`.`states`(`id`));

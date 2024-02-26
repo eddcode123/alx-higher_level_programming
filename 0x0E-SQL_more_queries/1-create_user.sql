b97b56fadbdece57386c8a45528cad7ff4c89e1f
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-26 12:28:15 +0300

    create user and assign all privilages

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -3,6 +3,3 @@ CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
 
 -- assign all privileges to user
 GRANT *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-
--- apply the changes immediately.
-FLUSH PRIVILEGES;

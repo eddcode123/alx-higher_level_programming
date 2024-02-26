6834dc3bdb0ebedc435c0d3006d1694556c7d238
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-26 12:26:11 +0300

    create user and assign all privilages

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,8 @@
+-- Script creates a new user and assigns all privileges to user
+CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- assign all privileges to user
+GRANT *.* TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- apply the changes immediately.
+FLUSH PRIVILEGES;

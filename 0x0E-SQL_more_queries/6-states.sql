ada4b30f4138f0ddda08d05984b521c1897fffa1
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-02-26 13:12:13 +0300

    adding script that creates the database hbtn_0d_usa and the table states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on a MySQL server.
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+-- create a table in the created data base
+CREATE TABLE IF NOT EXIST hbtn_0d_usa.states(
+    id INT UNIQUE NOT NULL AUTO_INCREMENT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
+);

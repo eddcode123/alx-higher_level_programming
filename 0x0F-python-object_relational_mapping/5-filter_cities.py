d405713733a0714a6fb5e1f9937184c9c5d74775
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-02 14:51:03 +0300

    this commit adds script that returns names of cities when a state is passed

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+""" list all cities of the state passed """
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    # Create a connection to the database
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL:", e)
+        sys.exit(1)
+
+    # Get cursor
+    cur = db.cursor()
+
+    # Create a query
+    query = 'SELECT cities.name \
+    FROM cities JOIN states ON cities.state_id = states.id \
+    WHERE states.name = %s ORDER BY cities.id ASC'
+
+    # Execute query
+    try:
+        cur.execute(query, (sys.argv[4],))
+        cities = cur.fetchall()
+        print(", ".join([city[0] for city in cities]))
+
+    except MySQLdb.Error as e:
+        print("Error executing query:", e)
+        sys.exit(1)
+
+    # Close the cursor
+    cur.close()
+
+    # Close the connection
+    db.close()

4964a066b4af292469497f38bfc18c76a5c8776b
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-02 13:33:48 +0300

    adding script safe from sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" displays all values in the states where name == argv[4] """
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    # Create a connection to the database
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL:", e)
+        sys.exit(1)
+
+    # Get cursor
+    cur = db.cursor()
+
+    # Execute query
+    try:
+        cur.execute("SELECT * \
+        FROM states \
+        WHERE CONVERT(`name` USING Latin1) \
+        COLLATE Latin1_General_CS = %s;", (sys.argv[4],))
+        results = cur.fetchall()
+        # Print results
+        for row in results:
+            print(row)
+    except MySQLdb.Error as e:
+        print('Query execution error:', e)
+        sys.exit(1)
+
+    # Close cursor
+    cur.close()
+    # Close connection to the database
+    db.close()

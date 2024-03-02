4b80bad7d48d473500cb17795c62e018549755dc
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-02 12:17:02 +0300

    adding a python script

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python3
+""" list all states in the data base hbtn_0e_0_usa """
 import MySQLdb
 import sys
 
-# Create a connection to the database
-try:
-    db = MySQLdb.connect(
-        host='localhost',
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        db=sys.argv[3],
-        port=3306
-    )
-except MySQLdb.Error as e:
-    print("Error connecting to MySQL:", e)
-    sys.exit(1)
+if __name__ == '__main__':
+    # Create a connection to the database
+    try:
+        db = MySQLdb.connect(
+            host='localhost',
+            user=sys.argv[1],
+            passwd=sys.argv[2],
+            db=sys.argv[3],
+            port=3306
+        )
+    except MySQLdb.Error as e:
+        print("Error connecting to MySQL:", e)
+        sys.exit(1)
 
-# Get cursor
-cur = db.cursor()
+    # Get cursor
+    cur = db.cursor()
 
-# Create a query
-query = 'SELECT * FROM states ORDER BY id ASC'
+    # Create a query
+    query = 'SELECT * FROM states ORDER BY id ASC'
 
-# Execute query
-try:
-    cur.execute(query)
-    result = cur.fetchall()
-    # Print results
-    for row in result:
-        print(row)
-except MySQLdb.Error as e:
-    print("Error executing query:", e)
-    sys.exit(1)
+    # Execute query
+    try:
+        cur.execute(query)
+        result = cur.fetchall()
+        # Print results
+        for row in result:
+            print(row)
+    except MySQLdb.Error as e:
+        print("Error executing query:", e)
+        sys.exit(1)
 
-# Close the cursor
-cur.close()
+    # Close the cursor
+    cur.close()
 
-# Close the connection
-db.close()
+    # Close the connection
+    db.close()

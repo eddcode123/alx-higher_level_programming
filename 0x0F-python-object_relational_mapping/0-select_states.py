dc559a6b5777e79fef3cde6d4a753cf945fa2d0d
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-02 11:42:08 +0300

    adding a python script

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+""" script that lists all states from the database hbtn_0e_0_usa """
+
+
+# create a list of arguments
+arguments = sys.argv[1:]
+
+# create a conection to the database
+db = MySQLdb.connect(host='localhost', user=arguments[0], passwd=arguments[1], db=arguments[2], port=3306)
+# get cursor
+cur = db.cursor()
+
+# create a query
+query = 'SELECT * FROM states ORDER BY id ASC'
+# execute query
+cur.exectute(query)
+result = cur.fetchall()
+# print results
+for row in result:
+    print(row)
+
+# close the cursor
+cur.close()
+# close the connection
+db.close()

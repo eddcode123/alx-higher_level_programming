d2445e30460626701656a79d0b5df8e5f198d0d6
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-03 14:17:26 +0300

    this commitids for a python script that list first states in a database using sqlalchemy

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -24,7 +24,7 @@ if __name__ == '__main__':
     state = session.query(State).order_by(State.id).first()
 
     # print first state
-    if state:
-        print(f'{state.id}: {state.name}')
-    else:
+    if state is None:
         print('None')
+    else:
+        print(f'{state.id}: {state.name}')

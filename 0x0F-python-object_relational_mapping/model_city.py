3ef7425fdb8f1de15d514e8ee276f0121c43232d
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-03 16:38:22 +0300

    this commit adds a model class that represents a city object

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,7 +5,7 @@ Define a city class and link to database.
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base
+from model_state import Base, State
 
 
 class City(Base):
@@ -13,7 +13,8 @@ class City(Base):
     Class represents citys of a state.
 
     """
+    
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)

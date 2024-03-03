a42a3403b5da90e4b0bc43c3f1db93cef6bfd59f
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-03 15:52:03 +0300

    this commit is for a python script that delete all states containing the letter a

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+deletes all State objects with a name containing the letter a from the database
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    # create a connection string
+    connection_string = (
+        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/'
+        f'{sys.argv[3]}'
+        )
+
+    # create an engine
+    engine = create_engine(connection_string, pool_pre_ping=True)
+    # create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # delete states containing letter a
+    delete_states = session.query(State).filter(State.name.like('%a%')).all()
+    for state in delete_states:
+        session.delete(state)
+    # commit transaction
+    session.commit()
+
+    # close session
+    session.close()

a357db86b3343be4b8cb6d1d379af198eeae3f1d
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-03 13:46:35 +0300

    this commitids for a python script that list all states in a database using sqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,21 +1,32 @@
 #!/usr/bin/python3
 """
-list all states object from a database
+List all states object from a database.
 """
 
-from model_state import Base, State
+import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
+from model_state import Base, State
 
-if __name__ == '__main__':
-    # create a connection string
+
+def main():
+    """
+    Main function to list all states from the database.
+    """
+    # Create a connection string
     connection_string = f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}'
-    # create an engine
+
+    # Create an engine
     engine = create_engine(connection_string)
-    # create a session
+
+    # Create a session
     Session = sessionmaker(bind=engine)
     session = Session()
-    # query to list all states
+
+    # Query to list all states
     for state in session.query(State).order_by(State.id):
         print(f'{state.id}: {state.name}')
+
+
+if __name__ == '__main__':
+    main()

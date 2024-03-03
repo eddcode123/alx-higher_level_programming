ebee744760546658ca1aeaf34cc69630bef56310
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-03 13:59:19 +0300

    this commitids for a python script that list all states in a database using sqlalchemy

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
 """
-List all states object from a database.
+list all states object from a database
 """
 
-import sys
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
-
-
-def main():
-    """
-    Main function to list all states from the database.
-    """
-    # Create a connection string
-    connection_string = f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/{sys.argv[3]}'
-
-    # Create an engine
-    engine = create_engine(connection_string)
-
-    # Create a session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Query to list all states
-    for state in session.query(State).order_by(State.id):
-        print(f'{state.id}: {state.name}')
-
+import sys
 
 if __name__ == '__main__':
-    main()
+    # create a connection string
+    connection_string = (
+        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/'
+        f'{sys.argv[3]}'
+        )
+
+    # create an engine
+    engine = create_engine(connection_string)
+    # create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # query to list all states
+    for state in session.query(State).order_by(State.id):
+        print(f'{state.id}: {state.name}')

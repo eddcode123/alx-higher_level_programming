f7fadc9f8f1768e250ebcb6ee9f8fe62f7f962ec
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-03 11:45:50 +0300

    this commit if for a model class thats represents states

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 """ Define a state class and link to data base """
-from sqlalchemy import Column, Integer, String, MetaData
+
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
-    """ Represent States """
-
+    """ Class represents States """
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, autoincrement='auto', unique=True, nullable=False)
     name = Column(String(128), nullable=False)

98316df4b8f5e4fb2721e943fb806ba44efdef2b
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-03 14:10:30 +0300

    this commitids for a python script that list first states in a database using sqlalchemy

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+prints the first state object from a database
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import sys
+
+if __name__ == '__main__':
+    # create a connection string
+    connection_string = (
+        f'mysql+mysqldb://{sys.argv[1]}:{sys.argv[2]}@localhost:3306/'
+        f'{sys.argv[3]}'
+        )
+
+    # create an engine
+    engine = create_engine(connection_string)
+    # create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    # query to list all states
+    state = session.query(State).first()
+
+    # print first state
+    print(f'{state.id}: {state.name}')

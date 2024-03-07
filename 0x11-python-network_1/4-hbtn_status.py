5b8604a6f69e78a563b5bc8c955841a4efc481ae
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-07 15:45:45 +0300

    This commit message is for a script that fetches url and prints body contents as required

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+
+if __name__ == '__main__':
+    # fetch url using get request
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    print('Body response:')
+    print(f'\ttype: {type(r.__reduce__())}')
+    print(f'\tcontent: {r.content}')

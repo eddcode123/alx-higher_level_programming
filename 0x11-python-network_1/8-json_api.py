7c3d6afec4394dd5cd0f17743e5bec00b2d281a8
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-07 17:26:53 +0300

    This commit message is for a script that takes a letter and sends a post request converts response to  JSON

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""  script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter
+"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    url = 'http://0.0.0.0:5000/search_user'
+    req = requests.post(url, data={'q': q})
+    try:
+        j_dict = req.json()
+    except ValueError:
+        print('Not a valid JSON')
+        sys.exit(1)
+    if j_dict:
+        print(f'[{j_dict['id']}] {j_dict['name']}')
+    else:
+        print('No result')

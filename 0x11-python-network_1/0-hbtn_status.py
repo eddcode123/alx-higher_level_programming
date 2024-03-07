3335475e978e4155e8f529545a6a03ed555be00e
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-07 13:29:51 +0300

    this commit is for a script that fetches a website

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,9 +8,10 @@ req = urllib.request.Request("https://alx-intranet.hbtn.io/status")
 try:
     with urllib.request.urlopen(req) as response:
         utf8_content = response.read().decode('utf-8')
+        content = print(response.read())
         print("Body response:")
         print("\t- type:", type(response))
-        print("\t- content:", response.read())
+        print("\t- content:", content)
         print("\t- utf8 content:", utf8_content)
 except urllib.error.URLError as e:
     print(e.reason)

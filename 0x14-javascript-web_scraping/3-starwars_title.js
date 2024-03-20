a35f57b06d8731b67d2bd76b31958f0a61d65caa
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-20 19:57:21 +0300

    this commit message is for a javascript code that uses request to get the title of a starwars film from a given api and takes the id from commandline

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 const request = require('request');
-const { argv } = require('process');
+const id = process.argv[2];
 
 const Url = 'https://swapi-api.alx-tools.com/api';
-request(Url + '/films/' + argv[2], (error, response, body) => {
+request(Url + '/films/' + id, (error, response, body) => {
   if (error) {
     console.error(error);
   }

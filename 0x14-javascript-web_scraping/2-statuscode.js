d91d270def4d0a9cc09925e933b9837f2144b9b2
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-20 19:20:38 +0300

    This commit message is for a script that takes url a commandline argument and sends a get request to the url and prints the response code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,9 +3,9 @@
 const request = require('request');
 const myUrl = process.argv[2];
 const resp = request(myUrl, (err, response) => {
-  if (err) {
-    console.error(err);
-    return;
-  }
-  console.log(`code: ${response.statusCode}`);
+    if (err){
+        console.error(err);
+        return;
+    }
+    console.log(`code: ${response.statusCode}`)
 });

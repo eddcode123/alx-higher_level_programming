3df275263e2aa883a43fc02f9c888d21c961d328
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-22 15:55:53 +0300

    this commit message is for javascript code that changes the color of html header when ser clicks on the tag DIV#red_header

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-$(document).ready(function(){
-    $('#red_header').click(function(){
-        $('header').css('color', 'red');
-    });
+$(document).ready(function () {
+  $('#red_header').click(function () {
+    $('header').css('color', 'red');
+  });
 });

5f056beb22d887c7e10128ae418535ca1b10a248
eddy123-sudo <janetwanjikumwangi05@gmail.com>
2024-03-22 15:55:36 +0300

    Thiscommit messageis for a javascript code that adds item to ul list when user clicks on the tag DIV#add_item

--- 0x15-javascript-web_jquery/5-script.js
+++ 0x15-javascript-web_jquery/5-script.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 $(document).ready(function () {
-    $('#add_item').click(function () {
-        $('.my_list').append('<li>Item</li>')
-    });
+  $('#add_item').click(function () {
+    $('.my_list').append('<li>Item</li>');
   });
+});
